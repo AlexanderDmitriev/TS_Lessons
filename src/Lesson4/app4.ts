3d370ffc296d4b8832ea694bbb494bcffb470c17
AlexanderDmitriev <alex9110077@gmail.com>
2022-07-11 17:57:03 +0300

    Update app4.ts

--- src/Lesson4/app4.ts
+++ src/Lesson4/app4.ts
@@ -1,26 +1,27 @@
-/* 
-Создайте объект Person
-
-1) конструктор принимает ключ класса Key и сохраняет его в свойство key
-2) метод getKey возвращает key
-
-Сделайте так, чтобы жилец попал домой. */
-
 /* Давайте построим дом */
 
 /* Создайте объект Key */
 class Key {
-    private signature!:number /* есть только свойство signature */
-    /* во время создания объекта генерирует случайное число и сохраняет его в signature */
-    constructor(){
-        this.signature = Math.random();
-    };
-/* метод getSignature возвращает случайное число из signature */
-    public getSignature(){
-        return this.signature;
-    };
+  private signature: number; /* есть только свойство signature */
+  /* во время создания объекта генерирует случайное число и сохраняет его в signature */
+  constructor() {
+    this.signature = Math.random();
+  }
+  /* метод getSignature возвращает случайное число из signature */
+  public getSignature() {
+    return this.signature;
+  }
 }
-class Person {}
+class Person {
+  /* конструктор принимает ключ класса Key и сохраняет его в свойство key */
+  constructor(private key: Key) {
+    this.key = key;
+  }
+  /* метод getKey возвращает key */
+  getKey = ():Key => {
+    return this.key;
+  };
+}
 
 enum doorType {
   OPENED,
@@ -39,21 +40,40 @@ abstract class House {
   }
   /*  метод comeIn, который добавляет объект класса Person в свойство tenants 
 и это срабатывает только если door открыта. */
-  comeIn = (person: Person) => {
+  comeIn = (person: Person):void => {
     if (this.door === doorType.OPENED) {
       this.tenants.push(person);
     }
   };
   /* абстрактный метод openDoor принимает аргумент класса Key */
   abstract openDoor(key: Key): boolean;
-};
+}
 
 /* Создайте класс MyHouse который реализует класс House */
-class MyHouse extends House{
-    /* создаем метод openDoor, так как он принимает ключ, 
+class MyHouse extends House {
+  /* создаем метод openDoor, так как он принимает ключ, 
 сверяем сохраненный ключ в свойстве key равен ли он ключу из аргумента, 
 если да, открываем дверь */
-    openDoor(key: Key): boolean {
-        return this.key.getSignature()===key.getSignature();
-    }
+  openDoor(key: Key): boolean {
+    return this.key.getSignature() === key.getSignature();
+  }
 }
+const myKey = new Key();
+const myHome = new MyHouse(doorType.CLOSED, myKey);
+/* Создайте объект Person */
+const somePerson = new Person(myKey);
+
+/* Сделайте так, чтобы жилец попал домой. */
+
+const entering = (someOne: Person, someHouse: MyHouse): void => {
+  if (someHouse.door === doorType.CLOSED) {
+    if (someHouse.openDoor(someOne.getKey())) {
+      console.log("Yes");
+    } else console.log("Oh, no");
+  } else {
+    someHouse.comeIn(someOne);
+    console.log("Yes");
+  }
+};
+
+entering(somePerson,myHome);
